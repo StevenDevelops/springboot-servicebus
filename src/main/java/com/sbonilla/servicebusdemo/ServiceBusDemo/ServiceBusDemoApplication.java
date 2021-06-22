f3cc2a60e0670668684aa58b53c4a38b1448a1d2
Steven <sbonilla@microsoft.com>
2021-06-22 01:36:18 -0400

    pom.xml : added azure-spring-boot-starter-servicebus-jms by pressing command + N and then search for artifact servicebus-jms

---
+++ src/main/java/com/sbonilla/servicebusdemo/ServiceBusDemo/ServiceBusDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sbonilla.servicebusdemo.ServiceBusDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBusDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBusDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbonilla/servicebusdemo/ServiceBusDemo/ServiceBusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbonilla.servicebusdemo.ServiceBusDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBusDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
