e77ff8ce778e0b29cd60483c4909d36c583b97cd
Steven <sbonilla@microsoft.com>
2021-06-22 03:19:06 -0400

    Successfully wrote to Service Bus queue using this command : curl -X POST 'http://localhost:8080/messages?message=finallyWORKS'

--- src/main/java/com/sbonilla/servicebusdemo/ServiceBusDemo/SendController.java
+++ src/main/java/com/sbonilla/servicebusdemo/ServiceBusDemo/SendController.java
@@ -14,25 +14,21 @@ public class SendController {
 
     private static final String DESTINATION_NAME = "sbonilla-demo";
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SendController.class);
+    private static final Logger logger = LoggerFactory.getLogger(SendController.class);
 
     @Autowired
     private JmsTemplate jmsTemplate;
 
-
-
-    /*
     @PostMapping("/messages")
     public String postMessage(@RequestParam String message) {
-        LOGGER.info("Sending message");
+        logger.info("Sending message");
         jmsTemplate.convertAndSend(DESTINATION_NAME, new User(message));
         return message;
     }
 
-    */
-
     @RequestMapping("/healthcheck")
     public String index() {
+        logger.info("run health check");
         return "Hello world, application healthy";
     }
 }
